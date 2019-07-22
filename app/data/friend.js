dd70ae289dba1e8051758b7f880ae250f32ad10a
Donirvin Dickerson <Dond2018@gmail.com>
2019-07-22 14:09:47 -0700

    update

---
+++ app/data/friend.js
@@ -0,0 +1,18 @@
+var friendArray = [
+    {
+        "name":"Will Smith",
+        "photo":"https://www.imdb.com/name/nm0000226/?ref_=nmls_pst",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+]
