aa011d1178050c6de283ba3a49b56244c39fafd9
Donirvin Dickerson <Dond2018@gmail.com>
2019-07-23 16:00:03 -0700

    Updated Survey script

--- app/data/friend.js
+++ app/data/friend.js
@@ -36,148 +36,148 @@ var friendArray = [
     ]
   },
 
-  // 3) Chris Hemsworth
-  {
-    "name": "Chris Hemsworth",
-    "photo": "https://m.media-amazon.com/images/M/MV5BOTU2MTI0NTIyNV5BMl5BanBnXkFtZTcwMTA4Nzc3OA@@._V1_UX140_CR0,0,140,209_AL_.jpg",
-    "scores": [
-      5,
-      1,
-      4,
-      4,
-      2,
-      5,
-      4,
-      5,
-      4,
-      1
-    ]
-  },
+  // // 3) Chris Hemsworth
+  // {
+  //   "name": "Chris Hemsworth",
+  //   "photo": "https://m.media-amazon.com/images/M/MV5BOTU2MTI0NTIyNV5BMl5BanBnXkFtZTcwMTA4Nzc3OA@@._V1_UX140_CR0,0,140,209_AL_.jpg",
+  //   "scores": [
+  //     5,
+  //     1,
+  //     4,
+  //     4,
+  //     2,
+  //     5,
+  //     4,
+  //     5,
+  //     4,
+  //     1
+  //   ]
+  // },
 
-  // 4) Lupita Nyong'o
-  {
-    "name": "Lupita Nyong'o",
-    "photo": "https://m.media-amazon.com/images/M/MV5BMTY0NTQ4MDY2Nl5BMl5BanBnXkFtZTgwNDk1MTEyMDE@._V1_UX140_CR0,0,140,209_AL_.jpg",
-    "scores": [
-      5,
-      1,
-      4,
-      4,
-      2,
-      5,
-      4,
-      5,
-      4,
-      1
-    ]
-  },
+  // // 4) Lupita Nyong'o
+  // {
+  //   "name": "Lupita Nyong'o",
+  //   "photo": "https://m.media-amazon.com/images/M/MV5BMTY0NTQ4MDY2Nl5BMl5BanBnXkFtZTgwNDk1MTEyMDE@._V1_UX140_CR0,0,140,209_AL_.jpg",
+  //   "scores": [
+  //     5,
+  //     1,
+  //     4,
+  //     4,
+  //     2,
+  //     5,
+  //     4,
+  //     5,
+  //     4,
+  //     1
+  //   ]
+  // },
 
-  // 5) Jason Momoa
-  {
-    "name": "Jason Momoa",
-    "photo": "https://m.media-amazon.com/images/M/MV5BMTI5MTU5NjM1MV5BMl5BanBnXkFtZTcwODc4MDk0Mw@@._V1_UX100_CR0,0,100,100_AL_.jpg",
-    "scores": [
-      5,
-      1,
-      4,
-      4,
-      2,
-      5,
-      4,
-      5,
-      4,
-      1
-    ]
-  },
+  // // 5) Jason Momoa
+  // {
+  //   "name": "Jason Momoa",
+  //   "photo": "https://m.media-amazon.com/images/M/MV5BMTI5MTU5NjM1MV5BMl5BanBnXkFtZTcwODc4MDk0Mw@@._V1_UX100_CR0,0,100,100_AL_.jpg",
+  //   "scores": [
+  //     5,
+  //     1,
+  //     4,
+  //     4,
+  //     2,
+  //     5,
+  //     4,
+  //     5,
+  //     4,
+  //     1
+  //   ]
+  // },
 
-  // 6) Gal Gadot
-  {
-    "name": "Gal Gadot",
-    "photo": "https://m.media-amazon.com/images/M/MV5BMjUzZTJmZDItODRjYS00ZGRhLTg2NWQtOGE0YjJhNWVlMjNjXkEyXkFqcGdeQXVyMTg4NDI0NDM@._V1_UY209_CR34,0,140,209_AL_.jpg",
-    "scores": [
-      5,
-      1,
-      4,
-      4,
-      2,
-      5,
-      4,
-      5,
-      4,
-      1
-    ]
-  },
+  // // 6) Gal Gadot
+  // {
+  //   "name": "Gal Gadot",
+  //   "photo": "https://m.media-amazon.com/images/M/MV5BMjUzZTJmZDItODRjYS00ZGRhLTg2NWQtOGE0YjJhNWVlMjNjXkEyXkFqcGdeQXVyMTg4NDI0NDM@._V1_UY209_CR34,0,140,209_AL_.jpg",
+  //   "scores": [
+  //     5,
+  //     1,
+  //     4,
+  //     4,
+  //     2,
+  //     5,
+  //     4,
+  //     5,
+  //     4,
+  //     1
+  //   ]
+  // },
 
-  // 7) Zoe Saldana
-  {
-    "name": "Chris Evans",
-    "photo": "https://m.media-amazon.com/images/M/MV5BMTM2MjIwOTc0Nl5BMl5BanBnXkFtZTcwNzQ5ODM1Mw@@._V1_UX140_CR0,0,140,209_AL_.jpg",
-    "scores": [
-      5,
-      1,
-      4,
-      4,
-      2,
-      5,
-      4,
-      5,
-      4,
-      1
-    ]
-  },
+  // // 7) Zoe Saldana
+  // {
+  //   "name": "Chris Evans",
+  //   "photo": "https://m.media-amazon.com/images/M/MV5BMTM2MjIwOTc0Nl5BMl5BanBnXkFtZTcwNzQ5ODM1Mw@@._V1_UX140_CR0,0,140,209_AL_.jpg",
+  //   "scores": [
+  //     5,
+  //     1,
+  //     4,
+  //     4,
+  //     2,
+  //     5,
+  //     4,
+  //     5,
+  //     4,
+  //     1
+  //   ]
+  // },
 
-  // 8) Keanu Reeves
-  {
-    "name": "Keanu Reeves",
-    "photo": "https://m.media-amazon.com/images/M/MV5BNzQxNDc5NzQ1MF5BMl5BanBnXkFtZTgwMTczMDExMzE@._V1_UY99_CR30,0,99,99_AL_.jpg",
-    "scores": [
-      5,
-      4,
-      1,
-      2,
-      4,
-      3,
-      2,
-      4,
-      5,
-      2
-    ]
-  },
+  // // 8) Keanu Reeves
+  // {
+  //   "name": "Keanu Reeves",
+  //   "photo": "https://m.media-amazon.com/images/M/MV5BNzQxNDc5NzQ1MF5BMl5BanBnXkFtZTgwMTczMDExMzE@._V1_UY99_CR30,0,99,99_AL_.jpg",
+  //   "scores": [
+  //     5,
+  //     4,
+  //     1,
+  //     2,
+  //     4,
+  //     3,
+  //     2,
+  //     4,
+  //     5,
+  //     2
+  //   ]
+  // },
 
-   // 9) Daniel Kaluuya
-   {
-    "name": "Daniel Kaluuya",
-    "photo": "https://m.media-amazon.com/images/M/MV5BOTk1MzgzOTg5OV5BMl5BanBnXkFtZTcwNDQ4NjMxOA@@._V1_UY209_CR1,0,140,209_AL_.jpg",
-    "scores": [
-      5,
-      4,
-      1,
-      2,
-      4,
-      3,
-      2,
-      4,
-      5,
-      2
-    ]
-  },
+  //  // 9) Daniel Kaluuya
+  //  {
+  //   "name": "Daniel Kaluuya",
+  //   "photo": "https://m.media-amazon.com/images/M/MV5BOTk1MzgzOTg5OV5BMl5BanBnXkFtZTcwNDQ4NjMxOA@@._V1_UY209_CR1,0,140,209_AL_.jpg",
+  //   "scores": [
+  //     5,
+  //     4,
+  //     1,
+  //     2,
+  //     4,
+  //     3,
+  //     2,
+  //     4,
+  //     5,
+  //     2
+  //   ]
+  // },
 
-   // 10) Henry Cavil
-   {
-    "name": "Henry Cavil",
-    "photo": "https://m.media-amazon.com/images/M/MV5BMTUxNTExMzUzOF5BMl5BanBnXkFtZTgwOTI1MjA3OTE@._V1_UX140_CR0,0,140,209_AL_.jpg",
-    "scores": [
-      5,
-      4,
-      4,
-      5,
-      4,
-      3,
-      5,
-      4,
-      5,
-      1
-    ]
-  }
+  //  // 10) Henry Cavil
+  //  {
+  //   "name": "Henry Cavil",
+  //   "photo": "https://m.media-amazon.com/images/M/MV5BMTUxNTExMzUzOF5BMl5BanBnXkFtZTgwOTI1MjA3OTE@._V1_UX140_CR0,0,140,209_AL_.jpg",
+  //   "scores": [
+  //     5,
+  //     4,
+  //     4,
+  //     5,
+  //     4,
+  //     3,
+  //     5,
+  //     4,
+  //     5,
+  //     1
+  //   ]
+  // }
 
 ]
