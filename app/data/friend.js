a795cb3971ade2040aefee650e9e2e2c0e7dee5a
Donirvin Dickerson <Dond2018@gmail.com>
2019-07-23 15:20:46 -0700

    Updated

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,18 +1,183 @@
 var friendArray = [
-    {
-        "name":"Will Smith",
-        "photo":"https://www.imdb.com/name/nm0000226/?ref_=nmls_pst",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      }
+  // 1) Will smith
+  {
+    "name": "Will Smith",
+    "photo": "https://m.media-amazon.com/images/M/MV5BNTczMzk1MjU1MV5BMl5BanBnXkFtZTcwNDk2MzAyMg@@._V1_UY209_CR2,0,140,209_AL_.jpg",
+    "scores": [
+      2,
+      2,
+      1,
+      4,
+      1,
+
+      5,
+      2,
+      5,
+      5,
+      3
+    ]
+  },
+  // 2) Chris Evans
+  {
+    "name": "Chris Evans",
+    "photo": "https://m.media-amazon.com/images/M/MV5BMTU2NTg1OTQzMF5BMl5BanBnXkFtZTcwNjIyMjkyMg@@._V1_.jpg",
+    "scores": [
+      2,
+      3,
+      3,
+      2,
+      3,
+    
+      1,
+      4,
+      5,
+      1,
+      4
+    ]
+  },
+
+  // 3) Chris Hemsworth
+  {
+    "name": "Chris Hemsworth",
+    "photo": "https://m.media-amazon.com/images/M/MV5BOTU2MTI0NTIyNV5BMl5BanBnXkFtZTcwMTA4Nzc3OA@@._V1_UX140_CR0,0,140,209_AL_.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      2,
+      5,
+      4,
+      5,
+      4,
+      1
+    ]
+  },
+
+  // 4) Lupita Nyong'o
+  {
+    "name": "Lupita Nyong'o",
+    "photo": "https://m.media-amazon.com/images/M/MV5BMTY0NTQ4MDY2Nl5BMl5BanBnXkFtZTgwNDk1MTEyMDE@._V1_UX140_CR0,0,140,209_AL_.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      2,
+      5,
+      4,
+      5,
+      4,
+      1
+    ]
+  },
+
+  // 5) Jason Momoa
+  {
+    "name": "Jason Momoa",
+    "photo": "https://m.media-amazon.com/images/M/MV5BMTI5MTU5NjM1MV5BMl5BanBnXkFtZTcwODc4MDk0Mw@@._V1_UX100_CR0,0,100,100_AL_.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      2,
+      5,
+      4,
+      5,
+      4,
+      1
+    ]
+  },
+
+  // 6) Gal Gadot
+  {
+    "name": "Gal Gadot",
+    "photo": "https://m.media-amazon.com/images/M/MV5BMjUzZTJmZDItODRjYS00ZGRhLTg2NWQtOGE0YjJhNWVlMjNjXkEyXkFqcGdeQXVyMTg4NDI0NDM@._V1_UY209_CR34,0,140,209_AL_.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      2,
+      5,
+      4,
+      5,
+      4,
+      1
+    ]
+  },
+
+  // 7) Zoe Saldana
+  {
+    "name": "Chris Evans",
+    "photo": "https://m.media-amazon.com/images/M/MV5BMTM2MjIwOTc0Nl5BMl5BanBnXkFtZTcwNzQ5ODM1Mw@@._V1_UX140_CR0,0,140,209_AL_.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      2,
+      5,
+      4,
+      5,
+      4,
+      1
+    ]
+  },
+
+  // 8) Keanu Reeves
+  {
+    "name": "Keanu Reeves",
+    "photo": "https://m.media-amazon.com/images/M/MV5BNzQxNDc5NzQ1MF5BMl5BanBnXkFtZTgwMTczMDExMzE@._V1_UY99_CR30,0,99,99_AL_.jpg",
+    "scores": [
+      5,
+      4,
+      1,
+      2,
+      4,
+      3,
+      2,
+      4,
+      5,
+      2
+    ]
+  },
+
+   // 9) Daniel Kaluuya
+   {
+    "name": "Daniel Kaluuya",
+    "photo": "https://m.media-amazon.com/images/M/MV5BOTk1MzgzOTg5OV5BMl5BanBnXkFtZTcwNDQ4NjMxOA@@._V1_UY209_CR1,0,140,209_AL_.jpg",
+    "scores": [
+      5,
+      4,
+      1,
+      2,
+      4,
+      3,
+      2,
+      4,
+      5,
+      2
+    ]
+  },
+
+   // 10) Henry Cavil
+   {
+    "name": "Henry Cavil",
+    "photo": "https://m.media-amazon.com/images/M/MV5BMTUxNTExMzUzOF5BMl5BanBnXkFtZTgwOTI1MjA3OTE@._V1_UX140_CR0,0,140,209_AL_.jpg",
+    "scores": [
+      5,
+      4,
+      4,
+      5,
+      4,
+      3,
+      5,
+      4,
+      5,
+      1
+    ]
+  }
+
 ]
