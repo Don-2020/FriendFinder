648e55e0d9d4478d512fa81dcfe784a89d2e89ea
Donirvin Dickerson <Dond2018@gmail.com>
2019-07-23 19:45:37 -0700

    Updated server

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+var apiRoutes = require('./app/routing/api-routes.js');
+var htmlRoutes = require('./app/routing/html-routes.js');
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+apiRoutes(app); // API route - Must be listed first due to the HTML default catch all "use" route
+htmlRoutes(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
