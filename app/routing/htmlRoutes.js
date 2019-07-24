c99f31d511772b7649402b227aaa39f97beff5c0
Donirvin Dickerson <Dond2018@gmail.com>
2019-07-23 19:35:55 -0700

    Updated Html route

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+var path = require('path');
+
+
+// Routing
+function htmlRoutes(app){
+    app.get('/survey', function(req,res){
+        res.sendFile(path.join(__dirname + '/../public/survey.html'))
+    });
+
+    app.use(function(req,res){
+        res.sendFile(path.join(__dirname + '/../public/home.html'));
+    });
+}
+// Export to main server
+module.exports = htmlRoutes;
